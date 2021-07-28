7fa335a52241e726c943d6e7cacece5c7b6cb296
Luis Herrera <luis.herrera@tinet.cl>
2021-07-28 10:32:04 -0400

    Refactor to read only active Task, id not done, but inactive.

--- src/main/java/cl/tinet/todolist/dao/TaskRepository.java
+++ src/main/java/cl/tinet/todolist/dao/TaskRepository.java
@@ -2,8 +2,10 @@ package cl.tinet.todolist.dao;
 
 import cl.tinet.todolist.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,10 +13,15 @@ import java.util.Optional;
  */
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("SELECT t FROM Task t WHERE t.active = TRUE")
+    List<Task> findAll();
+
     /**
      * find a task by id.
      *
      * @return Optional task.
      */
+    @Query("SELECT t FROM Task t WHERE t.id = ?1 and t.active = TRUE")
     Optional<Task> findById(Long id);
 }
