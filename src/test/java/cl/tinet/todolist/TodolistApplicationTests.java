ceae7569c2e7837743529bc73498ad5fd6fa1edc
Luis Herrera <luis.herrera@tinet.cl>
2021-08-03 11:48:40 -0400

    profile config 004

--- src/test/java/cl/tinet/todolist/TodolistApplicationTests.java
+++ src/test/java/cl/tinet/todolist/TodolistApplicationTests.java
@@ -5,11 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
+@ActiveProfiles("prod")
 class TodolistApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
